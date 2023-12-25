44367f14f64eeee72fd035994d333768056c0729
Sribastav Rajguru <prajguru54@gmail.com>
2023-12-25 21:29:47 +0530

    Added loggers

--- user_service/api/api_v1/endpoints/posts.py
+++ user_service/api/api_v1/endpoints/posts.py
@@ -36,19 +36,24 @@ def search_posts(
         f"&sortBy={sort_by}"
         f"&apiKey={api_key}"
     )
+    try:
+        response = requests.get(url)
+        response_json = response.json()
+        print(f"Response Status Code - {response.status_code}")
+        print(f"Response Headers - {response.headers}")
+        print(f"Status - {response_json['status']}")
+        print(f"totalResults - {response_json['totalResults']}")
 
-    response = requests.get(url)
-    response_json = response.json()
-    print(f"Status - {response_json['status']}")
-    print(f"totalResults - {response_json['totalResults']}")
+        articles = response_json.get("articles")
+        print(f"Article_1 - {articles[0]}")
 
-    articles = response_json.get("articles")
-    print(f"Article_1 - {articles[0]}")
-
-    return [
-        PostSearchResponse(**article, date_posted=article["publishedAt"])
-        for article in articles
-    ]
+        return [
+            PostSearchResponse(**article, date_posted=article["publishedAt"])
+            for article in articles
+        ]
+    except Exception as e:
+        print(f"Some error occurred: {str(e)}")
+        return
 
 
 @router.post("/", response_model=List[PostInDBResponse])
